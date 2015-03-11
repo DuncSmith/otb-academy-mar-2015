2d5b8646b15e69be9220317a6e8c1c6fa9018640
Duncan Smith <duncan.smith@onthebeach.co.uk>
2015-03-11 12:53:32 +0000

    Initial tests added

---
+++ hangman/hangman_spec.rb
@@ -0,0 +1,45 @@
+require_relative 'lib/hangman.rb'
+
+RSpec.describe "A game of hangman" do
+
+    it "creates a new game object" do
+    end
+
+    it "should accept a answer" do
+    end
+
+    it "will not let us guess if an answer has not yet been given" do
+    end
+
+    it "tell us the number of lives remaining" do
+    end
+
+    it "will not let us guess if no more lives available" do
+    end
+
+    it "will not let us guess if an answer has not yet been given" do
+    end
+
+    it "should decrement the number of lives after a incorrect guess" do
+    end
+
+    it "can tell us the answer if requested" do
+    end
+
+    it "can tell us if we have won" do
+    end
+
+    it "can tell us the guesses so far" do
+    end
+
+    it "can show us the game progress" do
+    end
+
+    it "will accept word guesses" do
+    end
+
+    it "can show us words guessed already" do
+    end
+
+
+end
