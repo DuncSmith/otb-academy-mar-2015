cbc154abc48a6212c929b451516c08c57660ec99
Duncan Smith <duncan.smith@onthebeach.co.uk>
2015-03-11 14:11:31 +0000

    first round of tests complete

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,11 +1,12 @@
 class Game
-  attr_reader :lguesses
+  attr_reader :lguesses, :wguesses
   attr_accessor :answer, :lives
 
-  def initialize(word)
-    @answer = word
+  def initialize(answer)
+    @answer = answer
     @lives = 15
     @lguesses = []
+    @wguesses = []
   end
 
   def lguess(letter)
@@ -13,9 +14,22 @@ class Game
     adjust_life(letter)
   end
 
+  def wguess(word)
+    @wguesses << word
+    @lives -= 1 if word != @answer
+  end
+
   def win?
+    letter_win? || word_win?
+  end
+
+  def letter_win?
     filter = @lguesses == [] ? " " : @lguesses
     @answer.gsub(/#{filter}/, "").size == 0
+  end
+
+  def word_win?
+    @wguesses.select { |w| w == @answer }.any?
   end
 
   def adjust_life(letter)
